53af1332218ee1c621834f54c5b11cbade46fcd0
umamaheswari-prakash <umamaheswarislm07@gmail.com>
2020-07-09 14:30:13 +0530

    argument assignment

---
+++ arguments_assignment.py
@@ -0,0 +1,13 @@
+def average(marks):
+    sum=0
+    for mark in marks:
+        sum =sum+mark
+    avg =sum/len(marks)
+    return avg
+x=average([80,96,84,70,56])
+print("mala","-",average([80,96,84,70,56]))
+
+
+
+
+
