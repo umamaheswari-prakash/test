381b852ce0908727054ac2abec1ad2ede0d57c93
umamaheswari-prakash <umamaheswarislm07@gmail.com>
2020-07-09 14:15:12 +0530

    module assignment

---
+++ module_assignment.py
@@ -0,0 +1,9 @@
+class my_name:
+     def __init__(self,name,x,y):
+         self.name = name
+         self.x=x
+         self.y=y
+     def about_name(self):
+         print("Hi,This is " + self.name )
+         print("x" +":"+self.x)
+         print("y" + ":" + self.y)
---
+++ module_output.py
@@ -0,0 +1,3 @@
+import module_assignment
+class_var= module_assignment.my_name("umamaheswari", "10","20")
+class_var.about_name()
